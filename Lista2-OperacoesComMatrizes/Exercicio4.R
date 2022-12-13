8dda29a966711baf887437e77b8f22efb16eac86
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-12-12 21:38:50 -0300

    Lista 2 Exercicio 4

---
+++ Lista2-OperacoesComMatrizes/Exercicio4.R
@@ -0,0 +1,11 @@
+"4) Dadas as matrizes A e B, calcule:"
+A<-matrix(c(5,4,2,3),byrow=T,nrow=2)
+"Matriz A"
+A
+B<-matrix(c(2,1,3,4,6,-1,5,1,0),byrow=T,nrow=3)
+"Matriz B"
+B
+"a) det(A)"
+5*3-2*4
+"b) det(B)"
+2*6*0+1*(-1)*5+3*4*1-3*6*5-2*(-1)*1-1*4*0
