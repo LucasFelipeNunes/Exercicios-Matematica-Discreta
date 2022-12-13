dad2c107c9a5b5c6b692358fa4a3a902b13c2216
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-12-13 13:30:18 -0300

    Lista 3 Inicio do Exercicio 3

---
+++ Lista3-ConjuntosNumericos/Exercicio3.R
@@ -0,0 +1,13 @@
+"3) Dados A = {0, 1, 2, 3}, B = {1, 2, 3} e C = {2, 3, 4, 5}, determine:"
+A = c(0,1,2,3)
+B = c(1,2,3)
+C = c(2,3,4,5)
+"a) A - B"
+setdiff(A,B)
+"b) A - C"
+setdiff(A,C)
+"c) B - C"
+setdiff(B,C)
+"d) A - vazio"
+A
+"e) "
