7712a95cd3cfd4244b5c8fe3ea10c832b608ac4b
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-12-12 21:29:37 -0300

    Lista 2 Exercicio 3

---
+++ Lista2-OperacoesComMatrizes/Exercicio3.R
@@ -0,0 +1,3 @@
+"3) Construa uma matriz A, quadrada de ordem 4, tal que aij e igual a i + 2j, se i for diferente de j, ou -3i - 2j, se i for igual a j."
+A<-matrix(c(-3-2*1,1+2*2,1+2*3,1+2*4,2+2,-3*2-2*2,2+2*3,2+2*4,3+2*1,3+2*2,-3*3-2*3,3+2*4,4+2*1,4+2*2,4+2*3,-3*4-2*4),byrow=T,nrow=4)
+A
